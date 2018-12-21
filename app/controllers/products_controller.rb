598d66e33c5bbf5efee4b4d26c63c6b0398d74eb
Denny Sander <sander.bewerbung@gmx.de>
2018-12-21 02:51:28 +0100

    Changed Color to Colour

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,6 +73,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url, :color, :prize, :quantity)
+      params.require(:product).permit(:name, :description, :image_url, :colour, :prize, :quantity)
     end
 end
