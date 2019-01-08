fb81b359545ae56f5bfd57f584d92ca94f6d7620
Denny Sander <sander.bewerbung@gmx.de>
2019-01-08 16:45:06 +0100

    CF Task 5.3 Mailers - Added a Contact form which responds with an automatic redirection to a thank_you-page and an email

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  post 'simple_pages/thank_you'
   get 'products/id:' => 'products#index'
   # root 'simple_pages#index
   root 'simple_pages#landing_page'
