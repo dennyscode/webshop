1d9d3cb0785ee1ee399a2c0a9038d760ba9e6cc2
Denny Sander <sander.bewerbung@gmx.de>
2019-01-08 22:22:50 +0100

    Fixed production.rb according to Ali´s solution! --> Putting STMP settings inside config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,19 @@
 config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :user_name            => ENV['gmail_username'],
- :password             => ENV['gmail_password'],
- :authentication       => "plain",
-:enable_starttls_auto => true
-}
-
 config.action_mailer.default_url_options = { host: 'enigmatic-meadow-72765.herokuapp.com' }
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
