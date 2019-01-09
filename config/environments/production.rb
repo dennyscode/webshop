c8cdf8fb3670de987bf5c2b47a95208e07f9301f
Denny Sander <sander.bewerbung@gmx.de>
2019-01-09 23:34:34 +0100

    Checking Gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['icefrager1@googlemail.com'],
+   :user_name            => ENV['icefrager1@googlemail.com_test'],
    :password             => ENV[''],
    :authentication       => "plain",
   :enable_starttls_auto => true
