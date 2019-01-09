0d0f1d3783d7a46987cec8f39f82a2c12bb35442
Denny Sander <sander.bewerbung@gmx.de>
2019-01-09 23:31:59 +0100

    fixing production.rb -> Adding Google Account info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => ENV['icefrager1@googlemail.com'],
+   :password             => ENV[''],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
