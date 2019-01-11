fa027e814a528fb8a793c31dedbaa012b357a2cf
Denny Sander <sander.bewerbung@gmx.de>
2019-01-11 12:17:57 +0100

    Fixing user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: "from@example.com"
 
-  def contact_form(email, name, message)
-    @message = message
-      mail(from: email,
-           to: 'sander.bewerbung@gmx.de',
-           subject: "A new contact form message from #{name}",
-           body: @message).deliver_now)
-  end
+def contact_form(email, name, message)
+@message = message
+  mail(from: email,
+       to: 'sander.bewerung@gmx.de',
+       subject: "A new contact form message from #{name}")
+end
 end
