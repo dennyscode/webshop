ab6e45213311c5eb2dfcbcd0ea987e2c4accd689
Denny Sander <sander.bewerbung@gmx.de>
2019-01-12 17:03:25 +0100

    Trying to fix the mailer- set to google

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "mail.gmx.net",
-   :port                 => 25,
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
    :user_name            => ENV["MAIL_USERNAME"],
    :password             => ENV["MAIL_PASSWORD"],
    :authentication       => "plain",
