346bd203574df792e20a3d09afd0b92e581d7c00
Denny Sander <sander.bewerbung@gmx.de>
2019-01-12 16:45:17 +0100

    Trying to fix the mailer- set port 587

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
    :address              => "mail.gmx.de",
-   :port                 => 465,
+   :port                 => 587,
    :user_name            => ENV["MAIL_USERNAME"],
    :password             => ENV["MAIL_PASSWORD"],
    :authentication       => "plain",
