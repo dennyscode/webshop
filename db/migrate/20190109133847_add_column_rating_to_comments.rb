101fb280a5ee9aecf43dc9c43409c97f591cf34f
Denny Sander <sander.bewerbung@gmx.de>
2019-01-13 20:50:13 +0100

    db:rollback --> before Adding Comments then Re-Creating the whole section

--- db/migrate/20190109133847_add_column_rating_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnRatingToComments < ActiveRecord::Migration[5.2]
-  def change
-    add_column :comments, :rating, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_134211) do
+ActiveRecord::Schema.define(version: 2019_01_13_194402) do
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
+    t.integer "user_id"
     t.text "body"
+    t.integer "rating"
     t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "rating"
-    t.integer "user_id"
     t.index ["product_id"], name: "index_comments_on_product_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
