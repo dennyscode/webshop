1143396564fc8807ad32a8bb228c8a493bae99b6
Denny Sander <sander.bewerbung@gmx.de>
2019-01-14 21:53:07 +0100

    Changing SMTP Login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
 
-
   config.before_configuration do
     env_file = File.join(Rails.root, 'config', 'local_env.yml')
     YAML.load(File.open(env_file)).each do |key, value|
