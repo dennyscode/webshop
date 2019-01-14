4dcd60ef9d0df7fe5329b8bd01b2b455f14b8815
Denny Sander <sander.bewerbung@gmx.de>
2019-01-14 21:29:34 +0100

    Changing SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
+   :address              => "w0152e18.kasserver.com",
    :port                 => 587,
    :user_name            => ENV["MAIL_USERNAME"],
    :password             => ENV["MAIL_PASSWORD"],
