55e54ef577bed2e4feabbcb0460bf94d1407cfb0
Denny Sander <sander.bewerbung@gmx.de>
2019-01-14 22:01:54 +0100

    Changing SMTP Login

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
-  default from: "sander.bewerbung@com"
+  default from: "d.sander@schaltglas.de"
 
 def contact_form(email, name, message)
 @message = message
   mail(from: email,
-       to: 'sander.bewerung@gmx.de',
+       to: 'sander.bewerbung@gmx.de',
        subject: "A new contact form message from #{name}")
 end
 end
