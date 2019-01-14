1878cfe078d29490914b87812b01b6bc375f254f
Denny Sander <sander.bewerbung@gmx.de>
2019-01-14 10:18:24 +0100

    CF 5.9 Add Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
   has_many :comments, dependent: :destroy
-
+  validates :name, presence: true
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
+  
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
@@ -12,7 +17,7 @@ class Product < ApplicationRecord
   def lowest_rating_comment
     comments.rating_desc.last
   end
-  
+
   def average_rating
     comments.average(:rating).to_f
   end
