ce4545725caef2806c68b8dcd122be49dc97fcea
Denny Sander <sander.bewerbung@gmx.de>
2019-02-01 13:11:13 +0100

    Restyling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
+
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
@@ -67,6 +68,7 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,15 @@
 class UserMailer < ApplicationMailer
   # default from: "d.sander@schaltglas.de"
 
-def contact_form(email, name, message)
-@message = message
-  mail(from: email,
-       to: 'sander.bewerbung@gmx.de',
-       subject: "A new contact form message from #{name}")
-end
+  def contact_form(email, name, message)
+  @message = message
+    mail(from: email,
+         to: 'sander.bewerbung@gmx.de',
+         subject: "A new contact form message from #{name}")
+  end
+
+  def welcome_send(user)
+    @user = user
+    mail to: user.email, subject: "Welcome to our Site", from: "info@mysite.com"
+  end
 end
