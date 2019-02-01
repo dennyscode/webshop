a22da62eed988329205a1de393c9502e9e874853
Denny Sander <sander.bewerbung@gmx.de>
2019-02-01 13:38:43 +0100

    Restyling - Routes Error Mailer:Welcome_Message

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'simple_pages/thank_you' => 'products#index'
   post 'simple_pages/thank_you'
   # post 'sign_up'
+  
   get 'products/id:' => 'products#index'
   # root 'simple_pages#index
   root 'simple_pages#landing_page'
